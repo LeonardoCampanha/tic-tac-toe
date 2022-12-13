9425c935e4440bae179a38a13927027270e7ab00
Leonardo Campanha <ileonardo@outlook.com>
2022-12-13 17:59:38 +0000

    finish v0.0.1

--- interface.js
+++ interface.js
@@ -10,19 +10,16 @@ function handleClick(event) {
 	let square = event.target;
 	let position = square.id;
 
-	handleMove(position);
-	updateSquares();
-	console.log(event.target.id);
+	if (handleMove(position)) {
+		setTimeout(() => {
+			alert("Game Over!!");
+		}, 100);
+	}
+	updateSquare(position);
 }
-function updateSquares() {
-	let squares = document.querySelectorAll(".square");
 
-	squares.forEach((square) => {
-		let position = square.id;
-		let symbol = board[position];
-
-		if (symbol != "") {
-			square.innerHTML = `<div class='${symbol}'></div>`;
-		}
-	});
+function updateSquare(position) {
+	let square = document.getElementById(position.toString());
+	let symbol = board[position];
+	square.innerHTML = `<div class='${symbol}'></div>`;
 }
